dae7e7c03dd662ded4bfa6c4c96d8f89314793a3
adadesigns <102214100+adadesigns@users.noreply.github.com>
2022-05-22 09:55:37 +0300

    Add project files.

---
+++ Graphs/Graphs.cpp
@@ -0,0 +1,67 @@
+//Grafuri orientate
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int a[50][50], i, j, n;
+ifstream f("graf.txt");
+
+
+//int a[50][50], i, j, n;
+//
+//int citireMatrice(int matrice[50][50])
+//{
+//	cout << "cate elem?";
+//	cin >> n;
+//	for ( i = 1; i <= n; i++)
+//		for ( j = 1; j <= n; j++)
+//		{
+//			cout << "a[" << i << "][" << j << "]= ";
+//			cin >> a[i][j];
+//			
+//		}
+//	return n;
+//}
+//
+void afisareMatrice(int matrice[50][50])
+{
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			cout << a[i][j]<<" ";
+
+		}
+		cout << endl;
+	}
+}
+
+//void calculGradNod(int matrice[50][50],int nrNoduri) {
+//
+//	
+//	for (i = 1; i <= nrNoduri; i++)
+//	{
+//		int suma = 0;
+//		for (j = 1; j <= nrNoduri; j++)
+//			suma = suma + matrice[i][j];
+//		cout << "gradul nodului " << i << "este: " << suma << endl;
+//	}
+//}
+int main()
+{
+	f >> n;
+	while (f >> i >> j) {
+	
+		a[i][j] = 1;
+
+	}
+
+	afisareMatrice(a);
+
+	//int nrNod = 0;
+	//nrNod=citireMatrice(a);
+	//afisareMatrice(a);
+	//calculGradNod(a, nrNod);
+}
+
+
